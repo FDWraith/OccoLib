194bdaa020e98d672f3a0bb238328770c744e1d3
Kevin Zhang <kevinzhang3702@gmail.com>
2016-09-23 12:32:24 -0400

    I made it look prettier

--- main.py
+++ main.py
@@ -4,15 +4,12 @@ import occupy
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    body = ""
-    return render_template('base.html', bd = body, d = [])
-
 @app.route("/occupations")
 def occupate():
-    lines = occupy.get_file()    
-    return render_template('base.html', bd = "Here is a great table", d = lines)
-
+    lines = occupy.get_file()
+    ranges = occupy.set_ranges(occupy.get_pcts(lines))
+    rndOccu = occupy.pull_rand_occ(ranges, occupy.get_occs(lines))
+    return render_template('base.html', bd = "", d = lines, header2 = "A Possible Career Path:", choice = rndOccu, header1 = "A List ofOccupations")
 
 if __name__ == "__main__":
     app.debug = True
